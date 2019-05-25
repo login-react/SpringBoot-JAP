85efba04753b5024c69dc4c792474e15b645ee54
login-react <ztm1762530@163.com>
2019-05-25 15:46:07 +0800

    增加注释

--- src/main/java/com/example/demo/Termal/service/IEmpService.java
+++ src/main/java/com/example/demo/Termal/service/IEmpService.java
@@ -8,6 +8,10 @@ import java.util.List;
  * Created by zhoutaimin on 2019/5/24.
  */
 public interface IEmpService {
+    /**
+     * 查询所有
+     * @return
+     */
     List queryEmpAll();
 
     Emp save(Emp emp);
